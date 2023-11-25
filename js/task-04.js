e03a6b2188c234a80672852bf85a686a9ed59b38
HrechanaAnaconda <141082045+HrechanaAnaconda@users.noreply.github.com>
2023-11-25 21:57:35 +0200

    renew_1

--- js/task-04.js
+++ js/task-04.js
@@ -1,178 +1,158 @@
 `use strict`;
-// function calculateTotal(number) {
-//   let sum = 0;
-//   for (let i = 0; i <= number; i++) {
-//     sum += i;
-//   }
-//   return sum;
+
+//TODO:==============================================
+/**
+*? Поверніть об'єкт, в якому вказано кількість тегів.
+*? Очікуваний результат {js: 3, nodejs: 3, html: 2, css: 2, react: 2}
+
+ */
+// const tweets = [
+//   { id: "000", likes: 5, tags: ["js", "nodejs"] },
+//   { id: "001", likes: 2, tags: ["html", "css"] },
+//   { id: "002", likes: 17, tags: ["html", "js", "nodejs"] },
+//   { id: "003", likes: 8, tags: ["css", "react"] },
+//   { id: "004", likes: 0, tags: ["js", "nodejs", "react"] },
+// ];
+
+///**
+// *? Напишіть функцію, яка приймає массив об'єктів і повертає новий массив
+// *? Зробіть знижку 20 % на всі фрукти у масиві
+// *? Надайте ід для кожного продукту
+// */
+
+// const fruits = [
+//   { name: "apple", price: 200 },
+//   { name: "orange", price: 300 },
+//   { name: "grapes", price: 750 },
+// ];
+
+// function updateFruits(arr) {
+//   return arr.map((items, index) => {
+//     items.price * 0.8;
+//     return { ...items, price: items.price * 0.8, id: index + 1 };
+//   });
 // }
 
-// console.log(calculateTotal(4));
+// console.log(updateFruits(fruits));
 
-// function calculateEvenTotal(number) {
-//   sum = 0;
-//   for (let i = 1; i <= number; i++) {
-//     if (i % 2 === 0) {
-//       sum += i;
-//     } else {
-//     }
-//   }
-
-//   return sum;
-// }
-
-// console.log(calculateEvenTotal(3));
-
-// const start = 6;
-// const end = 17;
-// let number;
-// for (number = start; number <= end; number++) {
-//   if (number % 5 === 0) {
-//     console.log(number);
-//     break;
-//   }
-// }
-// function findNumber(start, end, divisor) {
-//   for (let i = start; i <= end; i++) {
-//     if (i % divisor === 0) {
-//       return i;
-//     }
-//   }
-// }
-
-// const result = findNumber();
-// console.log(`Result of function execution ${result}`);
-// function getLastElementMeta(array) {
-//   const lastElementIndex = array.length - 1;
-//   const newArray = [lastElementIndex, array[lastElementIndex]];
-//   return newArray;
-// }
-//const result =  getLastElementMeta(["apple", "peach", "pear", "banana"]);
-// console.log(result);
-// function getLength(array) {
-//   const newArray = array.join("");
-//   return newArray.length;
-// }
-// const result = getLength(["Mango", "hurries", "to", "the", "train"]);
-// console.log(result);
-// function calculateEngravingPrice(message, pricePerWord) {
-//   const messageArray = message.split(" ");
-//   const sum = messageArray.length * pricePerWord;
-//   return sum;
-// }
-// const result = calculateEngravingPrice("JavaScript is in my blood", 10);
-// console.log(result);
-// const fruits = ["apple", "plum", "pear", "orange", "banana"];
-
-// const firstTwoEls = fruits.slice(0, 2);
-// const nonExtremeEls = fruits.slice(1, 4);
-// const lastThreeEls = fruits.slice(-3);
-
-// const result = firstTwoEls;
-// console.log(result);
-
-// const result2 = nonExtremeEls;
-// console.log(result2);
-
-// const result3 = lastThreeEls;
-// console.log(result3);
-// const oldClients = ["Mango", "Ajax", "Poly", "Kiwi"];
-// const newClients = ["Peach", "Houston"];
-// const allClients = oldClients.concat(newClients);
-// console.log(allClients);
-// function getSlice(array, value) {
-//   const newValue = array.indexOf(value);
-//   const newArray = array.slice(0, newValue + 1);
-//   return newArray;
-// }
-// const result = getSlice(["Mango", "Poly", "Ajax"], "Mango");
-// console.log(result);
-// function createArrayOfNumbers(min, max) {
-//   const numbers = [];
-//   for (let i = min; i <= max; i++) {
-//     numbers.push(`${i}`);
-//   }
-//   return numbers;
-// }
-// const result = createArrayOfNumbers(29, 34);
-// console.log(result);
-// const apartment = {
-//   imgUrl: "https://via.placeholder.com/640x480",
-//   descr: "Spacious apartment in the city center",
-//   rating: 4,
-//   price: 2153,
-//   tags: ["premium", "promoted", "top"],
-//   owner: {
-//     name: "Henry",
-//     phone: "982-126-1588",
-//     email: "henry.carter@aptmail.com",
-//   },
+/**
+ *? З об'єкту concerts потрібно отримати масив
+ *? в якому будуть лише імена міст.
+ *? З масиву потрібно прибрати міста, в яких концерт уже пройшов і
+ *? відсортувати їх у хронологічному порядку.
+ *? Результат вивести у консоль.
+ *? Очікуваний результат ["Одеса", "Умань", "Харків"]
+ */
+// const concerts = {
+//   Київ: new Date("2020-04-01"),
+//   Умань: new Date("2025-07-02"),
+//   Вінниця: new Date("2020-04-21"),
+//   Одеса: new Date("2025-03-15"),
+//   Хмельницький: new Date("2020-04-18"),
+//   Харків: new Date("2025-07-10"),
 // };
+// const arrayCity = Object.keys(concerts)
+//   .filter((city) => concerts[city] > new Date())
+//   .toSorted((a, b) => concerts[a] - concerts[b]);
+// console.log(arrayCity);
 
-// apartment.price = 5000;
-// apartment.rating = 4.7;
-// apartment.owner.name = "Henry Sibola";
-// apartment.tags.push("trushed");
+//TODO:=============================================
+// Створити клас Worker, у якого є властивості name, age, salary.
+//У класу Worker є метод getSalary.
+//Створити клас TopLevelWorker, у якого є властивість hierarchyLevel
+//і який успадковує клас Worker, додаючи метод getHierarchyLevel
 
-// console.log(apartment);
-// const apartment = {
-//   descr: "Spacious apartment in the city center",
-//   rating: 4,
-//   price: 2153,
-// };
-
-// const keys = [];
-// const values = [];
-
-// for (let key in apartment) {
-//   keys.push(key);
-//   values.push(apartment[key]);
+// class Worker {
+//   constructor(name, age, salary) {
+//     this.name = name;
+//     this.age = age;
+//     this.salary = salary;
+//   }
+//   getSalary() {
+//     return this.salary;
+//   }
 // }
 
-// console.log(keys);
-// console.log(values);
-//
-// const apartment = {
-//   descr: "Spacious apartment in the city center",
-//   rating: 4,
-//   price: 2153,
-// };
-// const values = [];
-// const keys = Object.keys(apartment);
-
-// for (let key of keys) {
-//   values.push(apartment[key]);
+// class TopLevelWorker extends Worker {
+//   constructor(hierarchyLevel, name, age, salary) {
+//     super(name, age, salary);
+//     this.hierarchyLevel = hierarchyLevel;
+//   }
+//   getHierarchyLevel() {
+//     return this.hierarchyLevel;
+//   }
 // }
-// console.log(keys);
-// console.log(values);
-// function countProps(object) {
-//   let propCount = Object.keys(object).length;
 
-//   return propCount;
+// const exempTopLevelWorker = new TopLevelWorker("top", "John", 35, 4500);
+// console.log(exempTopLevelWorker.getHierarchyLevel());
+// console.log(exempTopLevelWorker.getSalary());
+
+//TODO:=============================================
+/**
+ *? Напиши клас Notes який управляє колекцією нотаток у
+ *? властивості items.
+ *? Нотатка - це об'єкт із властивостями text і priority.
+ *? Додай класу статичний метод Priopity,
+ *? який буде повертати об'єкт із пріоритетами.
+ *? Додай методи addNote(note), removeNote(text)
+ *? updatePriority(text, newPriority)
+ */
+
+// class Notes {
+//   static Priority() {
+//     return {
+//       HIGHT: "hight",
+//       LOW: "low",
+//     };
+//   }
+//   #findNoteIndex(id) {
+//     return this.items.findIndex((item) => item.id === id);
+//   }
+//   constructor() {
+//     this.items = [];
+//   }
+//   addNote(note) {
+//     this.items.push(note);
+//   }
+//   removeNote(id) {
+//     const index = this.#findNoteIndex(id);
+//     if (index !== -1) this.items.splice(index, 1);
+//   }
+//   updatePriority(id, newPriority) {
+//     const index = this.#findNoteIndex(id);
+//     if (index !== -1) this.items[index].priority = newPriority;
+//   }
 // }
-// const result = countProps(apartment);
+// const newObj = new Notes();
+// newObj.addNote({ text: "aaa", priority: Notes.Priority().LOW, id: 1 });
+// newObj.addNote({ text: "bbb", priority: Notes.Priority().LOW, id: 2 });
+// newObj.updatePriority(2, Notes.Priority().HIGHT);
+// console.log(newObj);
+
+// *Напишіть функцію checkBrackets(str) яка приймає рядок жс коду (someFn)
+//   і перевіряє правильність закриття дужок () {} []
+//   Якщо рядок містить коректний код функція повертає true.
+//   В іншому випадку повертає false
+// */
+
+const someFn = `function foo() {
+  const arr = [1, 2, 3];
+  console.log(arr);
+}`;
+
+// console.log(checkBrackets(someFn));
+
+// function disemvowel(str) {
+//   return str.replace(/[aeiou]/gi, "");
+// }
+// const result = disemvowel(
+//   "This website is for losers LOL!)",
+//   "Ths wbst s fr lsrs LL!"
+// );
 // console.log(result);
-// const apartment = {
-//   descr: "Spacious apartment in the city center",
-//   rating: 4,
-//   price: 2153,
-// };
 
-// const keys = Object.keys(apartment);
-// const values = Object.values(apartment);
-// console.log(keys);
-// console.log(values);
+// function add(...args) {
+//   return args.reduce((acc, curr) => acc + curr, 0);
+// }
 
-function countTotalSalary(salaries) {
-  let totalSalary = 0;
-  const money = Object.values(salaries);
-
-  for (let coin of money) {
-    totalSalary += coin;
-    // console.log(totalSalary);
-  }
-  return totalSalary;
-}
-
-const result = countTotalSalary({ mango: 100, poly: 150, alfred: 80 });
-console.log(result);
+// console.log(add(15, 27));
